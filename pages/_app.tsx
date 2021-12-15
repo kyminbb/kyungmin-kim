2c650a8f4d3f2b7b6fd42c10423f9e3abe173e2c
Kyungmin Kim <kyminbb.kim@gmail.com>
2021-12-15 19:50:59 +0900

    Init next app

---
+++ pages/_app.tsx
@@ -0,0 +1,23 @@
+import { AppProps } from "next/app";
+import { NextPage } from "next";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import theme from "../utils/theme";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: Roboto, -apple-system, Ubuntu, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+`;
+
+const MyApp: NextPage<AppProps> = ({ Component, pageProps }: AppProps) => {
+	return (
+		<ThemeProvider theme={theme}>
+			<Component {...pageProps} />
+			<GlobalStyle />
+		</ThemeProvider>
+	);
+};
+
+export default MyApp;
